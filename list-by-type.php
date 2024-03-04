f8c5324f0bbbdf63f244e164d942dcda56a0f606
TiiMH <151692609+TiiMH@users.noreply.github.com>
2024-03-04 10:58:48 +0100

    listPokemon

--- list-by-type.php
+++ list-by-type.php
@@ -1,20 +1,35 @@
-<!-- 
-    Ce fichier représente la page de liste par type de pokémon du site.
--->
+<!-- Ce fichier représente la page de liste par type de pokémon du site -->
 
 <?php
 require_once("head.php");
 ?>
 
-<pre>
-    &lt;
-    A REMPLACER PAR VOTRE CODE POUR CHARGER ET AFFICHER DANS UN TABLEAU LA LISTE DES POKEMONS CLASSES PAR LEUR TYPE, PUIS PAR LEUR NOM.
-    CHAQUE POKEMON DOIT ETRE CLIQUABLE POUR NAVIGUER SUR UNE PAGE OU L'ON AFFICHE SON IMAGE ET L'ENSEMBLE DE SES CARACTERISTIQUES
-    &gt;
-    
-</pre>
-
 <?php
 require_once("footer.php");
+require_once ('config.php');
+$sql = "SELECT DISTINCT type FROM pokemons";
+$result = $conn->query($sql);
 ?>
+<?php while($row = $result->fetch_assoc()): ?>
+<h2><?php echo $row['type']; ?></h2>
+<table>
+    <tr>
+        <th>Numéro</th>
+        <th>Image</th>
+        <th>Nom</th>
+    </tr>
+    <?php
+    $sql2 = "SELECT * FROM pokemons WHERE type = '".$row['type']."' ORDER BY nom";
+    $result2 = $conn->query($sql2);
+    while($row2 = $result2->fetch_assoc()): ?>
+    <tr>
+        <td><?php echo $row2['id_pokemon']; ?></td>
+        <td><img src="<?php echo $row2['image']; ?>" alt="<?php echo $row2['nom']; ?>" width="50" height="50"></td>
+        <td><a href="pokemon_detail.php?id=<?php echo $row2['id_pokemon']; ?>"><?php echo $row2['nom']; ?></a></td>
+    </tr>
+    <?php endwhile; ?>
+</table>
+<?php endwhile; 
+?>
+
 
--- list.php
+++ list.php
@@ -1,25 +1,37 @@
-<!-- 
-    Ce fichier représente la page de liste de tous les pokémons.
--->
+<!--  Ce fichier représente la page de liste de tous les pokémons -->
 
 <?php
 require_once("head.php");
 require_once('database-connection.php');
 
-$query = $databaseConnection->query("SELECT NomPokemon, IdTypePokemon, IdSecondTypePokemon, urlPhoto from pokemon");
-if (!$query) {
-    throw new RuntimeException("Cannot execute query. Cause: " . mysqli_error($databaseConnection));
-} else {
-    $pokemons = $query->fetch_all(MYSQLI_ASSOC);
-    echo "<table border='1' cellpadding='10' cellspacing='0' style='border-collapse: collapse;'>";
-    echo "<tr><th>Nom</th><th>Type</th><th>Second Type</th><th>Image</th></tr>";
-    
-    foreach ($pokemons as $pokemon) {
-        echo "<tr><td>" . $pokemon["NomPokemon"] . "</td><td>" . $pokemon['IdTypePokemon'] . "</td><td>" . $pokemon["IdSecondTypePokemon"] . "</td><td><img src='" . $pokemon['urlPhoto']. "' alt='" . $pokemon["NomPokemon"] . "' width='50' height='50'></td></tr>";
+try {
+    $query = $databaseConnection->query("SELECT NomPokemon, urlPhoto, T.libelleType AS 'Type 1', T2.libelleType AS 'Type 2' from pokemon P JOIN typepokemon T ON P.IdTypePokemon = T.IdType LEFT JOIN typepokemon T2 ON P.IdSecondTypePokemon = T2.IdType ORDER BY IdPokemon");
+    if (!$query) {
+        throw new RuntimeException("Cannot execute query. Cause: " . mysqli_error($databaseConnection));
     }
+
+    $pokemons = $query->fetch_all(MYSQLI_ASSOC);
+
+    echo "<table border='1' cellpadding='10' cellspacing='0' style='border-collapse: collapse;'>";
+    echo "<tr>
+      <th>Nom</th>
+      <th>Type</th>
+      <th>SecondType</th>
+      <th>Image</th></tr>";
+
+    foreach ($pokemons as $pokemon) {
+        echo "<tr>";     
+        echo "<td>" . ($pokemon["NomPokemon"]) . "</td>";
+        echo "<td>" . ($pokemon['Type 1']) . "</td>";
+        echo "<td>" . ($pokemon["Type 2"]) . "</td>";
+        echo "<td><img src='" . ($pokemon['urlPhoto']) . "' alt='" . ($pokemon["NomPokemon"]) . "' width='50' height='50'></td>";
+        echo "</tr>";
+    }
+
     echo "</table>";
+} catch (Exception $e) {
+    echo "Une erreur s'est produite : " . ($e->getMessage());
+} finally {
+    require_once("footer.php");
 }
-require_once("footer.php");
-?> 
-
-
+?>
