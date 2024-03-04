bcdf1fd8e2f75197ef0b4d7c28fff0c7e774162f
TiiMH <151692609+TiiMH@users.noreply.github.com>
2024-03-04 09:24:37 +0100

    List.php

--- list.php
+++ list.php
@@ -1,17 +1,25 @@
 <!-- 
     Ce fichier représente la page de liste de tous les pokémons.
 -->
+
 <?php
 require_once("head.php");
-?>
-<pre>
-    &lt;
-    A REMPLACER PAR VOTRE CODE POUR CHARGER ET AFFICHER DANS UN TABLEAU LA LISTE DES POKEMONS PAR LEUR NOM.
-    CHAQUE POKEMON DOIT ETRE CLIQUABLE POUR NAVIGUER SUR UNE PAGE OU L'ON AFFICHE SON IMAGE ET L'ENSEMBLE DE SES CARACTERISTIQUES 
-    &gt;
-    </pre>
+require_once('database-connection.php');
 
-
-<?php
+$query = $databaseConnection->query("SELECT NomPokemon, IdTypePokemon, IdSecondTypePokemon, urlPhoto from pokemon");
+if (!$query) {
+    throw new RuntimeException("Cannot execute query. Cause: " . mysqli_error($databaseConnection));
+} else {
+    $pokemons = $query->fetch_all(MYSQLI_ASSOC);
+    echo "<table border='1' cellpadding='10' cellspacing='0' style='border-collapse: collapse;'>";
+    echo "<tr><th>Nom</th><th>Type</th><th>Second Type</th><th>Image</th></tr>";
+    
+    foreach ($pokemons as $pokemon) {
+        echo "<tr><td>" . $pokemon["NomPokemon"] . "</td><td>" . $pokemon['IdTypePokemon'] . "</td><td>" . $pokemon["IdSecondTypePokemon"] . "</td><td><img src='" . $pokemon['urlPhoto']. "' alt='" . $pokemon["NomPokemon"] . "' width='50' height='50'></td></tr>";
+    }
+    echo "</table>";
+}
 require_once("footer.php");
-?>
+?> 
+
+
